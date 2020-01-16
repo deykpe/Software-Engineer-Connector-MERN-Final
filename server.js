0c9ff51f18eded24855bfbd7fd733e82113e8c5b
deykpe <dpang2@uci.edu>
2020-01-15 17:00:21 -0800

    config.json mongoURI

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get('/', (res, req) => res.send('Api Running'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server starting on ${PORT}`));
