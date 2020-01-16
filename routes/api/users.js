8de6e5f9cce00605ad905e3849f64d9a012d77ea
deykpe <dpang2@uci.edu>
2020-01-16 12:54:40 -0800

    added jwt module

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 //user model
@@ -37,7 +39,9 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
       //get users gravatar
       const avatar = gravatar.url(email, {
@@ -57,7 +61,22 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
       await user.save();
       //return jsonwebtoken -- in order to log in right away, need token to login
-      res.send('User Route');
+      //   res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
