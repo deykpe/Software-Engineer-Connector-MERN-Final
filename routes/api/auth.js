af559b4a0a19688742c04c8129c8b28413132995
deykpe <dpang2@uci.edu>
2020-01-16 16:19:12 -0800

    fixed try catch function

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+
 const User = require('../../models/User');
 //@ route   GET api/auth
 //@ desc   Test route
 //access   Public
 router.get('/', auth, async (req, res) => {
   try {
-    const user = await (await User.findById(req.user.id)).select('-password');
+    const user = await User.findById(req.user.id).select('-password');
     res.json(user);
   } catch (err) {
     console.error(err.message);
