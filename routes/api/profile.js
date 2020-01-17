72e95124e6276bd9adc77cf84cdc00320f36bd44
deykpe <dpang2@uci.edu>
2020-01-17 14:42:34 -0800

    const object destructure

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -45,6 +45,23 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin
+    } = req.body;
+
+    //Profile object
+    const profileFields = {};
   }
 );
 module.exports = router;
