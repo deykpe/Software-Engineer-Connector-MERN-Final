6d6439f47f06099042ebea53c3c3c994f86cfd0a
deykpe <dpang2@uci.edu>
2020-01-16 17:08:44 -0800

    profifleschemas

--- models/Profile.js
+++ models/Profile.js
@@ -4,5 +4,28 @@ const ProfileSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user'
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
   }
 });
