07520aeadcef7cf3072ff938becdf4de9831199d
deykpe <dpang2@uci.edu>
2020-01-17 18:19:01 -0800

    if /return for array errors

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -174,6 +174,11 @@ router.put(
         .isEmpty()
     ]
   ],
-  async (req, res) => {}
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
 );
 module.exports = router;
