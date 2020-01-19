0158a595d95118f4aa51fb5ac9d3ae2c215ad190
deykpe <dpang2@uci.edu>
2020-01-18 20:36:45 -0800

    added post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -26,7 +26,23 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const user = await User;
+    try {
+      const user = await (await User.findById(req.user.id)).select('-password');
+
+      const newPost = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
