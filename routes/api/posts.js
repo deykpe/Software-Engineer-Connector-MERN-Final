d5d1bb08afdde9c2428d32b2ea941a057f21af0d
deykpe <dpang2@uci.edu>
2020-01-18 20:29:03 -0800

    posts route updated

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
-
-//@ route   GET api/posts
-//@ desc   Test route
-//access   Public
-router.get('/', (req, res) => res.send('Post Route'));
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+//@ route   POST api/posts
+//@ desc   Create a post
+//access   Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {}
+);
 
 module.exports = router;
