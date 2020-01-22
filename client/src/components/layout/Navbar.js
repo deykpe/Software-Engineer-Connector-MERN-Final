f5ef969385255ea273f9263d98ec4cca65d8f533
deykpe <dpang2@uci.edu>
2020-01-21 19:20:56 -0800

    alert components with redux and update

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className='navbar bg-dark'>
       <h1>
         <Link to='/'>
-          <i class='fas fa-code'></i> SeConnector
+          <i className='fas fa-code'></i> SeConnector
         </Link>
       </h1>
       <ul>
