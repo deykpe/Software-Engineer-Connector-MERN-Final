f4f0000fd1c598a0a3682c5589c84376cc5431f2
deykpe <dpang2@uci.edu>
2020-01-23 13:42:02 -0800

    created posts

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import Profiles from './components/profiles/Profiles';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
@@ -21,7 +22,7 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
-import { addEducation } from './actions/profile';
+// import { AddEducation } from './actions/profile';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -65,6 +66,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
