80800fb037cf92e122d19ae2820ed28d6812666c
deykpe <dpang2@uci.edu>
2020-01-23 00:07:52 -0800

    UI isnt actually rendering until the data is loaded

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import Profiles from './components/profiles/Profiles';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profile from './components/profile/Profile';
+
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -40,6 +42,7 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:id' component={Profile} />
 
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute
